0b70b1c6426439ef3a8c2b5acb6eb8f7cddaffbd
vvc132003 <vvc132003@gmail.com>
2025-04-20 20:48:32 +0700

    .

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { HomeListComponent } from './pages-home/home-list/home-list.component';
 
-const routes: Routes = [{ path: '', component: HomeComponent }];
-
+const routes: Routes = [
+  { path: '', component: HomeListComponent },
+];
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
+
+
 export class HomeRoutingModule { }
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,15 +3,29 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { LayoutHomeComponent } from './layout-home/layout-home.component';
+import { NavbarComponent } from './layout-home/navbar/navbar.component';
+import { HeaderComponent } from './layout-home/header/header.component';
+import { HomeListComponent } from './pages-home/home-list/home-list.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    LayoutHomeComponent,
+    NavbarComponent,
+    HeaderComponent,
+    HomeListComponent,
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    FormsModule,
+    ToastrModule.forRoot(),
+    HttpClientModule
   ]
 })
 export class HomeModule { }
